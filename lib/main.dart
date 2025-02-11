567dec9026aebc164ed942a4a0b11c2941bad6bc
Matthew Craig <matthewandrew.craig@calbaptist.edu>
2025-02-11 13:05:53 -0800

    Ok now its actually all completed with consts

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,32 @@
 import 'package:dnd_jr_design/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import '../widgets/bottom_navbar.dart';
+
 
 final ThemeData warlocksTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: Color(0xFF464538), // Dark olive green
   scaffoldBackgroundColor: Color(0xFF464538), // Deep earthy green
-  appBarTheme: AppBarTheme(
+  appBarTheme: const AppBarTheme(
     backgroundColor: Color(0xFF25291C),
     titleTextStyle: TextStyle(
         color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
     iconTheme: IconThemeData(color: Colors.white),
   ),
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
     backgroundColor: Color(0xFF25291C),
     selectedItemColor: Color.fromARGB(255, 238, 228, 207), // Warm beige highlight
     unselectedItemColor: Color.fromARGB(255, 159, 158, 154), // Muted grayish tone
   ),
-  drawerTheme: DrawerThemeData(
+  drawerTheme: const DrawerThemeData(
     backgroundColor: Color(0xFF25291C),
     
   ),
-  textTheme: TextTheme(
+  textTheme: const TextTheme(
     bodyMedium:
         TextStyle(color: Color(0xFFBFBBA5)), // Soft, readable beige text
   ),
-  cardColor: Color(0xFF464538), // Match primary color for consistency
-  iconTheme: IconThemeData(
+  cardColor: const Color(0xFF464538), // Match primary color for consistency
+  iconTheme: const IconThemeData(
       color: Color(0xFFD4C097)), // Matching accent color for icons
 );
 
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: warlocksTheme, // Apply the custom theme
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
