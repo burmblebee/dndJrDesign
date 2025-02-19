361a10906ea588bfcfbb8be8a5da77f1b119da86
Matthew Craig <matthewandrew.craig@calbaptist.edu>
2025-02-19 08:40:48 -0800

    Merging this branch into Matthew_Testing - campaign_screen feature is complete for now. NEeds backend hookups

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:warlocks_of_the_beach/screens/campaign_screen.dart';
 import 'package:warlocks_of_the_beach/screens/dnd_forms/character_name.dart';
-import 'firebase_options.dart'; // Ensure this file is generated properly
+import 'firebase_options.dart'; 
+
 
 
 void main() async {
@@ -56,7 +58,7 @@ class MyApp extends StatelessWidget {
           color: Color(0xFFD4C097), // Matching accent color for icons
         ),
       ),
-      home: const CharacterName(),
+      home: const CampaignScreen(),
     );
   }
 }
