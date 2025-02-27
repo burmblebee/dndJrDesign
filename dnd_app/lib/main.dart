c7ea955ce3530b351ee9cd9bb70108db8e748536
Gabriela Cisneros <Gabriela.Cisneros@calbaptist.edu>
2025-02-26 22:23:07 -0800

    I finished the Profile info and Personal info, and started preferance area

--- dnd_app/lib/main.dart
+++ dnd_app/lib/main.dart
@@ -65,20 +65,186 @@ class ProfilePage extends StatelessWidget {
                 indent: 20,
                 endIndent: 20,
               ),
+
               Padding(
                 padding: EdgeInsets.only(left:20),
-                child: Align(
-                alignment: Alignment.centerLeft,
-                child:Text(
-                    'Profile Information',
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 9, 0, 0)
+                child: Column(
+                  //makes it left aligned
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [
+                    Text(
+                      'Profile Information',
+                       style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255,9,0,0),
                        ),
-                  ),
-              ),
-                  ),
+                      ),
+
+                      //space between the title and row
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Username:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              'LuvleeGabs',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Name:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              'gabs',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                        ],
+                      ),
+
+                      //Personal Info area
+                       SizedBox(height: 10),
+                        Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                          endIndent: 20,
+                        ),
+                        Text('Personal Information',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255,9,0,0),
+                        ),
+                      ),
+                       SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Email:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              'gabriela.cisneros951@gmail.com',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Phone:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              '951-735-3499',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Gender:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              'Female',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                        ],
+                      ),
+
+                      //Preferenes area
+                       SizedBox(height: 10),
+                        Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                          endIndent: 20,
+                        ),
+                      Text(
+                      'Preferences',
+                       style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255,9,0,0),
+                       ),
+                      ),
+
+                       SizedBox(height: 3),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Mode:',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 9,
+                            child: Text(
+                              'Dark',
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ) ,
+                            ),
+                            const Expanded(child: Icon(Icons.arrow_forward_ios, size: 18, )),
+                        ],
+                      ),
+                  ],
+                ),
+                
+               ),
             ],
           ),
         ),
