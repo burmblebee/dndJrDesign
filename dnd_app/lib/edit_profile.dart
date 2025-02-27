d69b293da3014ef5c6986c29388f01e879fb8aed
Gabriela Cisneros <Gabriela.Cisneros@calbaptist.edu>
2025-02-27 12:00:47 -0800

    I started the edit profile screen and addit the default pfp and button to upload image

--- dnd_app/lib/edit_profile.dart
+++ dnd_app/lib/edit_profile.dart
@@ -1,13 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-//temporary
-class EditProfile extends StatelessWidget {
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  File? _image;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit Profile')),
       body: Center(
-        child: Text('This is the Edit Profile page'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _image != null
+                ? Image.file(
+                    _image!,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  )
+                : Image.asset(
+                    'assets/profile.png',
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 22, vertical: 6),
+                decoration: BoxDecoration(
+                  color: Color(0xFF25291C),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  "Import Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+
+            //divider + spacing
+            SizedBox(height: 10),
+            Divider(
+              color: Colors.grey,
+              thickness: 1,
+              indent: 20,
+              endIndent: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Column(
+                //makes it left aligned
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Information',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 9, 0, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
