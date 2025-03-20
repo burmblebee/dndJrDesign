b6710877b925850eab3e47445149a79356bcc139
Gabriela Cisneros <Gabriela.Cisneros@calbaptist.edu>
2025-03-19 20:39:51 -0700

    I mostly finished the apearance wise on schedule. I edited add_sesions' name and some setting so it could pass the dates  and events

--- dnd_app/lib/main.dart
+++ dnd_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Schedule(), 
+      home: Schedule(events: {},), 
     );
   }
 }
--- dnd_app/lib/schedule.dart
+++ dnd_app/lib/schedule.dart
@@ -1,44 +1,237 @@
+import 'package:dnd_app/add_session.dart';
 import 'package:flutter/material.dart';
-
+import 'package:dnd_app/event.dart';
 
 class Schedule extends StatefulWidget {
-  const Schedule({super.key});
+  final Map<DateTime, List<Event>> events; // Receiving events from AddSession
 
-  
+  const Schedule({super.key, required this.events});
 
   @override
   State<Schedule> createState() => _ScheduleState();
 }
 
 class _ScheduleState extends State<Schedule> {
- 
+  
+  //this is the function that will take you to the add session page
+  void _goToAddSession() async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddSession(),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
+    // Get all scheduled dates and sort them
+  List<DateTime> scheduledDates = widget.events.keys.toList();
+  scheduledDates.sort();
+
+  // Find the next scheduled session
+  DateTime? nextSession;
+  for (DateTime date in scheduledDates) {
+    if (date.isAfter(DateTime.now()) || date.isAtSameMomentAs(DateTime.now())) {
+      nextSession = date;
+      break;
+    }
+  }
+
+  // Format the next scheduled session date
+  String nextSessionText = nextSession != null
+      ? '${nextSession.month}/${nextSession.day}/${nextSession.year}'
+      : '';
+
     return Scaffold(
       backgroundColor: Color(0xFF464538),
+      //this is a placeholder appbar
       appBar: AppBar(
         backgroundColor: Color(0xFF25291C),
-        title: const Text('this is appbar',),
-      ),
-      body: Center(
-        child: Text(
-              'This is nothing',
-            ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor:Color(0xFF25291C),
-        items:const[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.white,),
-            label: 'Temp',
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            label: "Tempy",
-          ),
-        ]
+        title: const Text(
+          'this is appbar',
         ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+            ),
+          ),
+          //this is the image
+          Container(
+            height: 120,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/witch.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          //this is the next scheduled session text + date
+          Text(
+            'Next Scheduled Session: $nextSessionText',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 6),
+          //this is the return to campain button 
+          TextButton(
+            onPressed: _goToAddSession, 
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 26, vertical: 13),
+              decoration: BoxDecoration(
+                //the bg should probs be grey and white text
+                color: Color.fromARGB(255, 241, 187, 87),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: Text(
+                'Return to Campaign',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+            //this is the schedules session text + sessions + button
+          SizedBox(height: 10),
+          // Scheduled sessions container with centered content
+          Container(
+            height: 295,
+            width: 328,
+            decoration: BoxDecoration(
+              //should be grey bg with white text
+               color: Color.fromARGB(255, 241, 241, 192),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Scheduled Sessions text
+                Text(
+                  "Scheduled Sessions:",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                // If no events, show this message
+                widget.events.isEmpty
+                    ? Center(
+                        child: Text(
+                          'No scheduled sessions yet!',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    : Expanded(
+                        child: ListView.builder(
+                          itemCount: widget.events.length,
+                          itemBuilder: (context, index) {
+                            DateTime eventDate = widget.events.keys.elementAt(index);
+                            List<Event> eventsForDate = widget.events[eventDate]!;
+
+                            return Center(  // Centers the event item
+                              child: Container(
+                                margin: EdgeInsets.symmetric(vertical: 8),
+                                padding: EdgeInsets.all(8), // Half the height of the container
+                                width: 164, // Keep the width same as the parent container
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF25291C),
+                                  borderRadius: BorderRadius.circular(12),
+                                  border: Border.all(color: Colors.black),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,  // Ensures the date and events are centered
+                                  children: [
+                                    Text(
+                                      '${eventDate.toLocal()}'.split(' ')[0], // Format the date
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    ...eventsForDate.map((event) {
+                                      return Text(
+                                        event.title,
+                                        style: TextStyle(color: Colors.white),
+                                      );
+                                    }).toList(),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 10),
+          //will add session button
+          ElevatedButton(
+            onPressed: _goToAddSession, 
+            style: ElevatedButton.styleFrom(
+              // But background color
+              backgroundColor: Color.fromARGB(255, 241, 187, 87), 
+              // Text Color 
+              foregroundColor: Colors.black, 
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+            ),
+            child: Text('Add Session'),
+            ),
+          ],
+        ),
+        ),
+          SizedBox(height: 14),
+
+        ],
+      ),
+      //a place holder navbar
+      bottomNavigationBar:
+          BottomNavigationBar(backgroundColor: Color(0xFF25291C), items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          label: 'Temp',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.event),
+          label: "Tempy",
+        ),
+      ]),
     );
   }
 }
