5028ff2f14c8b67135718cd5c7f9e43d67fdf259
Michael Newbold <91293169+MichaelENewboldprivate@users.noreply.github.com>
2025-04-15 12:19:52 -0700

    Attempt modified android folder fix

--- android/app/src/main/kotlin/com/example/dnd_jr_design/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dnd_jr_design
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/warlocks_of_the_beach/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.warlocks_of_the_beach
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
